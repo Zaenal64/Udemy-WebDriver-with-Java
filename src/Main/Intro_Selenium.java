4e0f208f96c88992ecb5edc7197889047b25a3f7
zaenal fahmi <zaenalfahmi34@gmail.com>
2025-05-07 12:39:30 +0700

    First commit

---
+++ src/Main/Intro_Selenium.java
@@ -0,0 +1,21 @@
+package Main;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Intro_Selenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");	
+		System.out.println(driver.getTitle());
+		
+	
+	}
+
+}
