68744da591920b3a40cf7913b36ddfb7022050c0
zaenal fahmi <zaenalfahmi34@gmail.com>
2025-09-22 02:23:06 +0700

    add Configure TestNG

---
+++ src/TestNg/FirstSection.java
@@ -0,0 +1,17 @@
+package TestNg;
+
+import org.testng.annotations.Test;
+
+public class FirstSection {
+
+	@Test
+	public void Demo() {
+		System.out.println("Hello"); //automation
+	}
+	
+	@Test
+	public void SecondDemo() {
+		System.out.println("Bye");
+	}
+
+}
---
+++ src/TestNg/SecondSection.java
@@ -0,0 +1,11 @@
+package TestNg;
+import org.testng.annotations.Test;
+
+public class SecondSection {
+
+	@Test
+	public void test() {
+		System.out.println("Good");
+	}
+
+}
