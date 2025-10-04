64a08e018e3b3ac498a7fa0c674b854af972337d
zaenal fahmi <zaenalfahmi34@gmail.com>
2025-10-04 13:23:28 +0700

    Add TestNG
    1. Importance of xml file in Testing Configuration
    2. Prioritizing the testcases with TestNg
    3. Include and Exclude Mechanis, to control Testcases
    4. Executing th eTestcases at pacage level with regex

---
+++ src/TestNg/FourthSection.java
@@ -0,0 +1,24 @@
+package TestNg;
+
+import org.testng.annotations.Test;
+
+public class FourthSection {
+
+	@Test
+	public void WebloginHomeLoan() {
+		//Selenium
+		System.out.println("WebloginHome");
+	}
+	
+	@Test
+	public void MobileLoginHomeLoan() {
+		//Appium
+		System.out.println("MobileLoginHome");
+	}
+	
+	@Test
+	public void LoginAPIhomeLoan() {
+		//Rest Api Automation
+		System.out.println("ApiLoginHome");
+	}
+}
---
+++ src/TestNg/ThirdSection.java
@@ -0,0 +1,38 @@
+package TestNg;
+
+import org.testng.annotations.Test;
+
+public class ThirdSection {
+
+	@Test
+	public void WebloginCarLoan() {
+		//Selenium
+		System.out.println("WebloginCar");
+	}
+	
+	@Test
+	public void MobileLoginCarLoan() {
+		//Appium
+		System.out.println("MobileLoginCar");
+		
+	}
+	
+	@Test
+	public void MobilesigninCarLoan() {
+		//Appium
+		System.out.println("Mobile SIGNIN");
+		
+	}
+	@Test
+	public void MobilesignoutCarLoan() {
+		//Appium
+		System.out.println("Mobile SIGNOUT");
+		
+	}
+	
+	@Test
+	public void APIcarLoan() {
+		//Rest Api Automation
+		System.out.println("ApiLoginCar ");
+	}
+}
